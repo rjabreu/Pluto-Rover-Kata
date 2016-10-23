4f6a5696f90a37f3da04ee23c86004a16a9294b3
ricardo abreu <rj.abreu@outlook.com>
2016-10-23 15:00:14 +0100

    added methods to increment rover position in X/Y axis

--- PlutoRoverKata/Rover.cs
+++ PlutoRoverKata/Rover.cs
@@ -8,5 +8,40 @@ namespace PlutoRoverKata
 {
     public class Rover
     {
+        public int PositionY { get; set; }
+        public int PositionX { get; set; }
+        public string Heading { get; set; }
+
+
+        public Rover()
+        {
+            PositionX = 0;
+            PositionY = 0;
+            Heading = "N";
+        }
+
+        public void Move(string sentCommands)
+        {
+            string[] commands = sentCommands.Split();
+
+            foreach (var command in commands)
+            {
+                if (command.Equals("F") )
+                {
+
+                }
+            }
+        }
+
+        public void IncreasePosY()
+        {
+            PositionY++;
+        }
+
+        public void IncreasePosX()
+        {
+            PositionX++;
+        }
+
     }
 }
--- PlutoRoverKataTests/RoverTests.cs
+++ PlutoRoverKataTests/RoverTests.cs
@@ -1,13 +1,62 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlutoRoverKata;
 
 namespace PlutoRoverKataTests
 {
     [TestClass]
     public class RoverTests
     {
+        Rover rover;
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            rover = new Rover();
+        }
+
+
+
         [TestMethod]
-        public void TestMethod1()
+        public void CanMoveY()
+        {
+            rover.IncreasePosY();
+
+            Assert.IsTrue(rover.PositionY == 1);
+        }
+
+        [TestMethod]
+        public void CanMoveX()
+        {
+            rover.IncreasePosX();
+            Assert.IsTrue(rover.PositionX == 1);
+        }
+
+        [TestMethod]
+        public void MoveForward()
+        {
+
+
+        }
+
+        [TestMethod]
+        public void RoverIsNorth()
+        {
+            Assert.IsTrue(rover.Heading == "N");
+        }
+
+        [TestMethod]
+        public void MoveBackward()
+        {
+        }
+
+        [TestMethod]
+        public void TurnLeft()
+        {
+        }
+
+        [TestMethod]
+        public void TurnRight()
         {
         }
     }
